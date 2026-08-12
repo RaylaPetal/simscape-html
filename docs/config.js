0049d8a232975febd1d9ce0581eb2e20946c8314
Rayla Petal <raylapetal@gmail.com>
2026-08-12 15:15:34 -0300

    fix loading venues

--- docs/config.js
+++ docs/config.js
@@ -4,5 +4,5 @@
 // supabase/migrations/0002_public_view_and_rls.sql) restrict what it can read.
 window.SHOWSIMS_CONFIG = {
   SUPABASE_URL: "https://ingqyyxryrfunxgedypt.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluZ3F5eXhyeXJmdW54Z2VkeXB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY1NTE2MDUsImV4cCI6MjEwMjEyNzYwNX0.eAHXfvLSFTBCQH8jpu9LsVI6YNH1bBvZf3IIRebKXJQ,
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluZ3F5eXhyeXJmdW54Z2VkeXB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY1NTE2MDUsImV4cCI6MjEwMjEyNzYwNX0.eAHXfvLSFTBCQH8jpu9LsVI6YNH1bBvZf3IIRebKXJQ"
 };
